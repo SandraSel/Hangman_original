ed713a0583d863cc6c884aa0c0896087fbebf73e
Sandra Selicka <selicka.san@gmail.com>
2021-04-05 22:28:54 +0300

    Initial commit

---
+++ src/main/scala/Hangman.scala
@@ -0,0 +1,10 @@
+object Hangman extends App {
+println("Welcome to the word guessing game, i.e. Hangman. " +
+  "You have 10 guesses. " +
+  "Good luck!")
+  // FIXME MAKE IT SO ALL THE TEXT APPEARS IN A NEW LINE. As now it is still in in one line
+
+  readline to enter the guess?
+
+  //TODO maybe add an option to guess in different languages?
+}
---
+++ src/main/scala/Utilities.scala
@@ -0,0 +1,37 @@
+object Utilities extends App {
+
+  def englishWords(file: String = "englishWords.txt") : List[String] = {
+    val source = io.Source.fromFile(file)
+    val words: List[String] = source.getLines.toList
+    source.close()
+    words
+  }
+
+  // Return a random word
+  def randomWord(words : List[String]) : String = {
+    words( scala.util.Random.nextInt(words.length) )
+  }
+  // TODO ADD A SEED FOR TESTING
+
+  // Split a word into separate letters
+  def wordSplit(word : String) : List[Char] = {
+    word.toList
+  }
+
+  // Join the list of characters together with a space in-between.
+  def wordJoin(wordlist : List[Char]) : String = {
+    wordlist.mkString(" ")
+    //other option: wordlist.mkString(" ").toUpperCase
+  }
+
+  // Set of upper case letters.
+  // TODO Check if can be done only with the previous def comment
+  def alphaSet : Set[Char] = {
+    ('A' to 'Z').toSet
+  }
+
+  // Generate a new guess list based on letter, current matches and actual word.
+  def applyGuess(letter : Char, guesslist : List[Char], hanglist : List[Char]) : List[Char] = {
+    guesslist.zip(hanglist).map({case(g,h) => if (letter == h) h else g})
+  }
+}
