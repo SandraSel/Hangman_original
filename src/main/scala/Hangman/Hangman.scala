05a396a67af1b118e23d62747364ed005d2d5878
Sandra Selicka <selicka.san@gmail.com>
2021-04-20 12:02:45 +0300

    Difficulty levels and case class added

--- src/main/scala/Hangman/Hangman.scala
+++ src/main/scala/Hangman/Hangman.scala
@@ -1,71 +1,68 @@
+package Hangman
+
 import scala.io.StdIn.readLine
 
 object Hangman extends App {
-println("Welcome to the word guessing game - Hangman. " +
-  "You have 10 guesses. " +
-  "Good luck!")
 
+  println("Welcome to the word guessing game - Hangman. You have 10 guesses.")
 
+  val file = if (args.isEmpty) "./src/Resources/englishWords.txt" else args(0)
+  var guessWords = Utilities.englishWords(file, Utilities.setDifficulty())
 
-  val file_path = if (args.isEmpty) "./src/Resources/englishWords.txt" else args(0)
-  var guessWords = Utilities.englishWords(file_path)
+  //guessWords.foreach(println)
 
   //TODO maybe put them in a class?
-  val letters: Set[Char] = Utilities.alphaSet
-  val formattedPlayerInput = "\t%s [Guesses Left: %2d ] Letter: "
-  val summary = "%s Wins : %2d  Losses : %2d"
+  var state = GameState()
 
-  var endGame = false
-  var win = 0
-  var loss = 0
+  while (!state.end) {
 
-  while (!endGame){
-    readLine("Type 'Exit' to leave the game, 'New' for a new game!")
+    println("Type 'Exit' to leave the game, 'New' for a new game!")
+    
 
     val guessingWord = Utilities.randomWord(guessWords)
     guessWords = guessWords.filterNot(_ == guessingWord)
     val splitWord = Utilities.wordSplit(guessingWord.toUpperCase)
     var wordUnderscoreGuess = Utilities.wordSplit("_" * guessingWord.length) //shows the length of the word with underscores
-    var guessSet : Set[Char] = Set()
+    var guessSet: Set[Char] = Set()
     var guessCount = 10
 
     var newGame = false
-    while (!newGame && !endGame){
+    while (!newGame && !state.end) {
       def checkGuesses(): Unit = {
 
-        def printResult(message : String): Unit = {
+        def printResult(message: String): Unit = {
           println("\t" + Utilities.wordJoin(splitWord) + "\n")
-          println(summary.format(message, win, loss)) //https://docs.scala-lang.org/overviews/core/string-interpolation.html
+          println(state.summary.format(message, state.wins, state.losses)) //https://docs.scala-lang.org/overviews/core/string-interpolation.html
           //https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/Formatter.html#detail
           //https://alvinalexander.com/scala/scala-string-formatting-java-string-format-method/
         }
-  // if the player guesses the word correctly then if versions, else looses a move
+        // if the player guesses the word correctly then if versions, else looses a move
         if (splitWord == wordUnderscoreGuess) {
           newGame = true
-          win += 1
+          state.wins += 1
           printResult("Congratulations! You won!")
         } else {
           guessCount match {
             case 1 =>
               newGame = true
-              loss += 1
+              state.losses += 1
               printResult("Sorry, but try again! ;)")
             case _ => guessCount -= 1
           }
         }
       }
 
-      val playersInput = readLine(formattedPlayerInput.format(Utilities.wordJoin(wordUnderscoreGuess), guessCount)).toUpperCase
+      val playersInput = readLine(state.formattedInput.format(Utilities.wordJoin(wordUnderscoreGuess), guessCount)).toUpperCase
       playersInput match {
-        case "NEW" => guessCount = 1 ; checkGuesses()
-        case "EXIT" => endGame = true
+        case "NEW" => guessCount = 1; checkGuesses()
+        case "EXIT" => state.end = true
         case "" => Nil
         case _ =>
-          val letter : List[Char] = playersInput.toList
-          if ((1 < letter.length) || !letters.contains(letter.head)) {
+          val letter: List[Char] = playersInput.toList
+          if ((1 < letter.length) || !state.letters.contains(letter.head)) {
             println("It is not a valid guess ->" + playersInput)
           } else if (guessSet.contains(letter.head)) {
-            println ("You already guessed this! Try again!")
+            println("You already guessed this! Try again!")
           } else {
             if (splitWord.contains(letter.head)) {
               wordUnderscoreGuess = Utilities.applyGuess(letter.head, wordUnderscoreGuess, splitWord)
